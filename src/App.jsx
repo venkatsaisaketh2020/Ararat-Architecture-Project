748f70e258098fdd56773cb8d6aa4d0866df6549
venkatsaisaketh2020 <venkatsaisaketh.suragoni@gmail.com>
2025-07-01 15:07:49 +0530

    first changes

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import Home from "./Ararat Architecture/Home";
+import Navbar from "./Ararat Architecture/Navbar";
+
+const App = () => {
+    return(
+        <section className="app">
+            <article>
+                <Navbar/>
+                <Home/>
+            </article>
+        </section>
+    )
+}
+export default App;
